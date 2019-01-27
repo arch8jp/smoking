ddc15614aa564729436e28ca6341081d12ad3c16
oneokrock256 <one0ok0rock0@gmail.com>
2019-01-27 11:57:36 +0900

    スマホ版css変更。 前の検索結果が残ってたのでグローバル変数をなくしました。

--- app/index.js
+++ app/index.js
@@ -7,7 +7,8 @@ const ejs = require('ejs');
 const qs = require('querystring');
 
 const index_page = fs.readFileSync("./index.ejs", "utf8");
-const style_css = fs.readFileSync('./style.css', "utf8");
+const normalize_css = fs.readFileSync('./public/stylesheet/normalize.css', "utf8");
+const style_css = fs.readFileSync('./public/stylesheet/style.css', "utf8");
 
 var server = http.createServer(getFormClient);
 server.listen(3000);
@@ -19,17 +20,18 @@ console.log("Server Start!");
 // Googleで「data.msg」について検索する。
 
 
-var lat = "34.397667";
-var lng = "132.475379";
+
 
 function getLatLng(request, response, word) {
+    var lat = "34.397667";
+    var lng = "132.475379";
     client.fetch('https://www.geocoding.jp/', { q: word }, function (err, $, res, body) {
 
         //HTML緯度経度を表示ここで文字を抽出
         lat = $("span[class=latlng]").text().split(" ", 2)[1];
         lng = $("span[class=latlng] b:nth-child(2)").text();
 
-        write_index(request, response);
+        write_index(request, response, lat, lng, word);
     });
 
 }
@@ -40,15 +42,30 @@ function getFormClient(request, response) {
         case "/":
             response_index(request, response);
             break;
+        case "/normalize.css":
+            response.writeHead(200, { "Content-Type": "text/css" });
+            response.write(normalize_css);
+            response.end();
+            break;
         case "/style.css":
             response.writeHead(200, { "Content-Type": "text/css" });
             response.write(style_css);
             response.end();
             break;
         case "/images/back.png":
-            response.writeHead(200,{"Content-type":"text/png"});
-            response.end(fs.readFileSync("./images/back.png"));
+            response.writeHead(200, { "Content-type": "text/png" });
+            response.end(fs.readFileSync("./public/images/back.png"));
             break;
+
+        case "/images/back_mob.png":
+            response.writeHead(200, { "Content-type": "text/png" });
+            response.end(fs.readFileSync("./public/images/back_mob.png"));
+            break;
+        case "/images/smoking.png":
+
+            response.writeHead(200, { "Content-Type": "text/png" });
+            response.end(fs.readFileSync("./public/images/smoking.png"));
+
         default:
             response.writeHead(200, { "Content-Type": "text/plian" });
             response.end("no  page...");
@@ -56,9 +73,10 @@ function getFormClient(request, response) {
     }
 }
 
-var data = { msg: "広島駅" };
+
 
 function response_index(request, response) {
+    var data = { msg: "広島駅" };
     var body = "";
     if (request.method == "POST") {
         request.on("data", (data) => {
@@ -71,13 +89,13 @@ function response_index(request, response) {
 
         });
     } else {
-        write_index(request, response);
+        write_index(request, response, "34.397667", "132.475379", data.msg);
     }
 }
 
-function write_index(request, response) {
+function write_index(request, response, lat, lng, word) {
     var content = ejs.render(index_page, {
-        content: data.msg,
+        content: word,
         content1: lat,
         content2: lng,
         content3: 'https://www.google.com/maps/d/embed?mid=1nfJ4qICIxHyiVMtdFl2Un8iSBXtCVT2I&ll=' + lat + ',' + lng + '&z=18',
